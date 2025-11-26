1bc496e6ab199e5f7fa2108735fc81a4a2cd1c35
Adarsh Agrawal <adarsh.agrawal1@ibm.com>
2025-11-26 11:33:01 +0530

    Updating base to ubi-minimal for tools image
    Signed-off-by: Adarsh Agrawal <adarsh.agrawal1@ibm.com>

--- ai-services/internal/pkg/vars/var.go
+++ ai-services/internal/pkg/vars/var.go
@@ -5,7 +5,7 @@ import "regexp"
 var (
 	// SpyreCardAnnotationRegex -> ai-services.io/<containerName>--spyre-cards
 	SpyreCardAnnotationRegex = regexp.MustCompile(`^ai-services\.io\/([A-Za-z0-9][-A-Za-z0-9_.]*)--sypre-cards$`)
-	ToolImage                = "icr.io/ai-services-cicd/tools:0.2"
+	ToolImage                = "icr.io/ai-services-cicd/tools:0.3"
 	ModelDirectory           = "/var/lib/ai-services/models"
 )
 
