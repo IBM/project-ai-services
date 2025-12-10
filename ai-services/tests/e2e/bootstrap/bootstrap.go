59d3cc2b883b51a3f7c30684be25155ac760378e
Abhishek-Kushwaha <Abhishek.Kushwaha@ibm.com>
2025-12-10 07:51:39 +0000

    Adding config and bootstrap base utilities for e2e test
    Signed-off-by: Abhishek-Kushwaha <Abhishek.Kushwaha@ibm.com>

---
+++ ai-services/tests/e2e/bootstrap/bootstrap.go
@@ -0,0 +1,9 @@
+package bootstrap
+
+import "fmt"
+
+// Placeholder function
+func HealthCheck(baseURL string) error {
+	fmt.Println("[BOOTSTRAP] Placeholder: health check for", baseURL)
+	return nil
+}
