5621b8c4f3403520009f6979638a127bbc4084f5
mayuka-c <Mayuka.C@ibm.com>
2026-04-01 09:42:45 +0530

    Opensearch HA configurations
    Signed-off-by: mayuka-c <Mayuka.C@ibm.com>

--- spyre-rag/src/common/opensearch.py
+++ spyre-rag/src/common/opensearch.py
@@ -36,8 +36,13 @@ class OpensearchVectorStore(VectorStore):
         self.db_prefix = os.getenv("OPENSEARCH_DB_PREFIX", "rag").lower()
         i_name = os.getenv("OPENSEARCH_INDEX_NAME", "default")
         self.index_name = self._generate_index_name(i_name.lower())
+        
+        # Replication settings for multi-node clusters
+        self.num_shards = int(os.getenv("OPENSEARCH_NUM_SHARDS", "1"))
+        self.num_replicas = int(os.getenv("OPENSEARCH_NUM_REPLICAS", "0"))
 
         logger.debug(f"Connecting to OpenSearch at {self.host}:{self.port}, index: {self.index_name}")
+        logger.debug(f"Index configuration: shards={self.num_shards}, replicas={self.num_replicas}")
 
         self.client = OpenSearch(
             hosts=[{'host': self.host, 'port': self.port}],
@@ -96,7 +101,9 @@ class OpensearchVectorStore(VectorStore):
             "settings": {
                 "index": {
                     "knn": True,  # Enable k-NN search functionality
-                    "knn.algo_param.ef_search": 100  # Number of candidates to consider during search (higher = more accurate but slower)
+                    "knn.algo_param.ef_search": 100,  # Number of candidates to consider during search (higher = more accurate but slower)
+                    "number_of_shards": self.num_shards,  # Number of primary shards for data distribution
+                    "number_of_replicas": self.num_replicas  # Number of replica copies for high availability
                 }
             },
             "mappings": {
