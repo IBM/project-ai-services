4afefabf65423687c795d83909234956dc690eee
sagarwal-ibm <sagarwal@ibm.com>
2026-05-20 11:33:50 +0530

    Address comments - 1
    Signed-off-by: sagarwal-ibm <sagarwal@ibm.com>

--- ai-services/internal/pkg/catalog/apiserver/models/create_application.go
+++ ai-services/internal/pkg/catalog/apiserver/models/create_application.go
@@ -2,16 +2,15 @@ package models
 
 // CreateApplicationRequest represents the request body for creating a new application.
 type CreateApplicationRequest struct {
-	Name         string    `json:"name" binding:"required,min=3,max=100"`
-	Architecture string    `json:"architecture" binding:"required"`
-	Services     []Service `json:"services" binding:"required,dive"`
+	Name      string    `json:"name" binding:"required,min=3,max=100"`
+	CatalogID string    `json:"catalogid" binding:"required"`
+	Services  []Service `json:"services" binding:"required,dive"`
 }
 
 // Service represents a service configuration in the application.
 type Service struct {
 	Type       string      `json:"type" binding:"required,eq=service"`
 	ServiceID  string      `json:"service_id" binding:"required"`
-	Enabled    bool        `json:"enabled"`
 	Version    string      `json:"version"`
 	Components []Component `json:"components" binding:"required,dive"`
 }
