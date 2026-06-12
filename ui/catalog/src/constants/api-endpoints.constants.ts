28a0cda33fdf1765a6ee62175e676fef33f40e95
Niranjan Joshi <Niranjan.Joshi1@ibm.com>
2026-06-12 20:13:52 +0530

    feat(catalog-ui): Services Page (#870)
    **Changes**
    
    - Added services cards to display available services**
    <img width="1470" height="835" alt="Screenshot 2026-06-04 at 1 17 01 PM"
    src="https://github.com/user-attachments/assets/384c113e-3beb-4e28-9efe-327264d9bc41"
    />
    
    
    
    - Added dynamic button text to service cards based on the selected
    service
    <img width="1132" height="652" alt="image"
    src="https://github.com/user-attachments/assets/00712ccf-ca28-4e3f-9b0f-5453602925e1"
    />
    
    
    
    - Added a side details panel to view service-specific information
    <img width="2940" height="1660" alt="image"
    src="https://github.com/user-attachments/assets/ea18ab55-ea9d-4d2f-8f83-1a7c2257511a"
    />
    <img width="642" height="1606" alt="image"
    src="https://github.com/user-attachments/assets/2a7e7752-d4e6-44c4-9a53-adad6e961841"
    />
    
    
    
    - Added a table to display deployed services
    <img width="2940" height="1660" alt="image"
    src="https://github.com/user-attachments/assets/90065684-ab4c-480d-80dd-117650c39848"
    />
    
    ---------
    
    Signed-off-by: Niranjan Joshi <Niranjan.Joshi1@ibm.com>

--- ui/catalog/src/constants/api-endpoints.constants.ts
+++ ui/catalog/src/constants/api-endpoints.constants.ts
@@ -3,3 +3,13 @@ export const AUTH_ENDPOINTS = {
   LOGOUT: "/auth/logout",
   REFRESH: "/auth/refresh",
 };
+
+export const SERVICE_ENDPOINTS = {
+  GET_SERVICES: "/services",
+};
+
+export const APPLICATION_ENDPOINTS = {
+  GET_APPLICATIONS: "/applications",
+  GET_DEPLOYED_SERVICES: "/applications?deployment_type=services",
+  DELETE_APPLICATION: (id: string) => `/applications/${id}`,
+};
