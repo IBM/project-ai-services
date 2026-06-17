59e6569c775efc72e6ff69279baebd783033a022
sagarwal-ibm <sagarwal@ibm.com>
2026-06-17 14:30:34 +0530

    introduce legacy flag for stop cmd
    Signed-off-by: sagarwal-ibm <sagarwal@ibm.com>

--- ai-services/internal/pkg/application/types/types.go
+++ ai-services/internal/pkg/application/types/types.go
@@ -48,10 +48,10 @@ type StartOptions struct {
 
 // StopOptions contains parameters for stopping an application.
 type StopOptions struct {
-	Name         string
-	PodNames     []string
-	AutoYes      bool
-	Experimental bool
+	Name     string
+	PodNames []string
+	AutoYes  bool
+	Legacy   bool
 }
 
 // ListOptions contains parameters for listing applications.
