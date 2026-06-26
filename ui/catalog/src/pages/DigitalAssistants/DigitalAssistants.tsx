41348f090927a000aebd94be3fd46a96a3604dac
Pranith Rao <Pranith.Rao@ibm.com>
2026-06-26 11:25:28 +0530

    Both tables UI fix (#1058)
    https://jsw.ibm.com/browse/AISERVICES-1275 - Grey background fix in both
    the tables, this was because of the sticky behavior we added to the name
    and the actions column, removed them for now. (Verified both on Safari
    and Chrome)
    https://jsw.ibm.com/browse/AISERVICES-1307 - Fixed this column
    visibility issue in DA page. Works as expected now
    https://jsw.ibm.com/browse/AISERVICES-1276 - 'New' word was missing
    added that
    
    ---------
    
    Signed-off-by: Pranith Rao <pranith.rao@ibm.com>

--- ui/catalog/src/pages/DigitalAssistants/DigitalAssistants.tsx
+++ ui/catalog/src/pages/DigitalAssistants/DigitalAssistants.tsx
@@ -553,15 +553,20 @@ const DigitalAssistantsPage = () => {
                                                 <TableCell>
                                                   {child.name}
                                                 </TableCell>
-                                                <TableCell>
-                                                  <StatusCell
-                                                    value={child.status}
-                                                    rowId={child.id}
-                                                    dispatch={dispatch}
-                                                  />
-                                                </TableCell>
-                                                <TableCell />
-                                                <TableCell />
+                                                {state.visibleColumns
+                                                  .status && (
+                                                  <TableCell>
+                                                    <StatusCell
+                                                      value={child.status}
+                                                      rowId={child.id}
+                                                      dispatch={dispatch}
+                                                    />
+                                                  </TableCell>
+                                                )}
+                                                {state.visibleColumns
+                                                  .uptime && <TableCell />}
+                                                {state.visibleColumns
+                                                  .messages && <TableCell />}
                                                 <TableCell />
                                               </TableRow>
                                             ),
@@ -575,7 +580,7 @@ const DigitalAssistantsPage = () => {
                             {noApplications && (
                               <NoDataEmptyState
                                 title="Start by adding a digital assistant"
-                                subtitle="To deploy a digital assistant, click Deploy."
+                                subtitle="To deploy a new digital assistant, click Deploy."
                                 className={styles.noDataContent}
                               />
                             )}
