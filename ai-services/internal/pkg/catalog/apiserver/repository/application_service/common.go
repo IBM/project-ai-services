2261e14cbf15605df44ca996c587f81cd8b3a2df
sagarwal-ibm <sagarwal@ibm.com>
2026-07-27 18:00:01 +0530

    update images
    Signed-off-by: sagarwal-ibm <sagarwal@ibm.com>

--- ai-services/internal/pkg/catalog/apiserver/repository/application_service/common.go
+++ ai-services/internal/pkg/catalog/apiserver/repository/application_service/common.go
@@ -18,21 +18,6 @@ import (
 	"github.com/project-ai-services/ai-services/internal/pkg/logger"
 )
 
-// Error message constants for application operations.
-const (
-	// ErrMsgApplicationNotFound is returned when an application does not exist.
-	ErrMsgApplicationNotFound = "application does not exist"
-
-	// ErrMsgUserNotOwner is returned when a user does not own the application.
-	ErrMsgUserNotOwner = "user does not own this application"
-
-	// ErrMsgApplicationAlreadyDeleting is returned when an application is already being deleted.
-	ErrMsgApplicationAlreadyDeleting = "application is already being deleted"
-
-	// ErrMsgApplicationNameExists is returned when an application with the given name already exists.
-	ErrMsgApplicationNameExists = "application with name '%s' already exists"
-)
-
 // ValidationError represents a validation error with HTTP status code.
 type ValidationError = validators.ValidationError
 
