7f237f42ad76654937d1902f9d69110fae62ec7c
Adarsh Agrawal <adarsh.agrawal1@ibm.com>
2026-07-27 12:10:45 +0530

    Adding reset password for catalog oc runtime
    Signed-off-by: Adarsh Agrawal <adarsh.agrawal1@ibm.com>

--- ai-services/internal/pkg/runtime/interface.go
+++ ai-services/internal/pkg/runtime/interface.go
@@ -27,6 +27,7 @@ type Runtime interface {
 	ListSecrets(filters map[string][]string) ([]string, error)
 	DeleteSecret(name string) error
 	SecretExists(nameOrID string) (bool, error)
+	UpdateSecret(name string, data map[string][]byte) error
 
 	// Volume operations
 	DeleteVolume(name string) error
@@ -40,6 +41,10 @@ type Runtime interface {
 
 	// Network operations
 	ListRoutes() ([]types.Route, error)
+
+	// Deployment operations
+	RolloutRestartDeployment(name string) error
+	GetDeploymentStatus(name string) (*types.DeploymentStatus, error)
 
 	// PVC operations
 	DeletePVCs(appLabel string) error
