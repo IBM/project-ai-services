710cad0fcf01fb6f190ec8158a56e2b272087750
Niranjan Joshi <Niranjan.Joshi1@ibm.com>
2026-09-09 23:01:34 +0530

    Disable remove button when status is syncing
    Signed-off-by: Niranjan Joshi <Niranjan.Joshi1@ibm.com>

--- ui/catalog/src/components/DeploymentDetails/components/ApplicationDatasourcesTable/CellRenderers.tsx
+++ ui/catalog/src/components/DeploymentDetails/components/ApplicationDatasourcesTable/CellRenderers.tsx
@@ -27,18 +27,23 @@ export const LastSyncCell = ({ value }: Pick<CellRendererProps, "value">) => (
   <span>{String(value ?? "")}</span>
 );
 
-export const DeleteCell = ({ rowId, dispatch }: CellRendererProps) => (
-  <Button
-    hasIconOnly
-    kind="ghost"
-    size="sm"
-    renderIcon={TrashCan}
-    iconDescription="Remove"
-    onClick={() =>
-      dispatch({ type: "SHARED_OPEN_DELETE_DIALOG", payload: rowId })
-    }
-  />
-);
+export const DeleteCell = ({ rowId, dispatch, rowData }: CellRendererProps) => {
+  const isDisabled =
+    rowData?.status === "syncing" || rowData?.status === "delete pending";
+  return (
+    <Button
+      hasIconOnly
+      kind="ghost"
+      size="sm"
+      renderIcon={TrashCan}
+      iconDescription="Remove"
+      disabled={isDisabled}
+      onClick={() =>
+        dispatch({ type: "SHARED_OPEN_DELETE_DIALOG", payload: rowId })
+      }
+    />
+  );
+};
 
 export const CELL_RENDERERS: Record<string, RendererFn> = {
   status: StatusCell as RendererFn,
