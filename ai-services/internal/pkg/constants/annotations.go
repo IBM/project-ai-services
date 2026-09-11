289f03b509c73b4c81df5c38b10b6af5fe7b5d8c
sagarwal-ibm <sagarwal@ibm.com>
2026-09-11 14:35:34 +0530

    bump images
    Signed-off-by: sagarwal-ibm <sagarwal@ibm.com>

--- ai-services/internal/pkg/constants/annotations.go
+++ ai-services/internal/pkg/constants/annotations.go
@@ -10,5 +10,5 @@ const (
 	PrerequisiteLabelKey     = "ai-services.io/prerequisite"
 	ComponentLabelKey        = "ai-services.io/component-type"
 	EndpointTypeLabelKey     = "ai-services.io/endpoint-type"
-	PodComponentKey = "ai-services.io/component"
+	PodComponentKey          = "ai-services.io/component"
 )
